87f73076ce05dd20b868e5921d95afc92b3cea68
Westin Humble <westin.h@gmail.com>
2022-01-19 10:26:09 -0500

    first handoff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
 console.log('%c HI', 'color: firebrick')
+
+const firstFetch = fetch(imgUrl) 
+    .then(r => r.json())
+    .then(data => data.forEach(imgCallBack))
+
+    //firstFetch();
+
+
+const imgCallBack = function(dog) {
+    const dogImage = document.querySelector("#dog-image-container");
+    //dog.innerHTML = firstFetch
+    
+}
